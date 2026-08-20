5ab931dac42a6229bfc24a62f2bc58c754d38843
HotSpot2000-hub <HotSpot2000@gmx.net>
2026-08-20 21:07:20 +0200

    Disable native WebView overscroll

--- native/MainActivity.java
+++ native/MainActivity.java
@@ -1,6 +1,8 @@
 package de.playboy.archiv;
 
 import android.os.Bundle;
+import android.view.View;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -8,5 +10,14 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         registerPlugin(ArchiveDirectoryPlugin.class);
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            getBridge().getWebView().setOverScrollMode(View.OVER_SCROLL_NEVER);
+        }
     }
 }
