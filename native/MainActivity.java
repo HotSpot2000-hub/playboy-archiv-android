bc89929f5e42fffd24e8c48b8463d642f5f4f768
HotSpot2000-hub <HotSpot2000@gmx.net>
2026-08-20 15:07:55 +0200

    Add Android immersive mode

--- native/MainActivity.java
+++ native/MainActivity.java
@@ -1,6 +1,12 @@
 package de.playboy.archiv;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -8,5 +14,37 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         registerPlugin(ArchiveDirectoryPlugin.class);
         super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    }
+
+    public void enterImmersiveMode() {
+        Window window = getWindow();
+        WindowInsetsControllerCompat controller =
+                WindowCompat.getInsetsController(window, window.getDecorView());
+
+        if (controller != null) {
+            controller.hide(
+                    WindowInsetsCompat.Type.statusBars()
+                            | WindowInsetsCompat.Type.navigationBars()
+            );
+
+            controller.setSystemBarsBehavior(
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            );
+        }
+    }
+
+    public void exitImmersiveMode() {
+        Window window = getWindow();
+        WindowInsetsControllerCompat controller =
+                WindowCompat.getInsetsController(window, window.getDecorView());
+
+        if (controller != null) {
+            controller.show(
+                    WindowInsetsCompat.Type.statusBars()
+                            | WindowInsetsCompat.Type.navigationBars()
+            );
+        }
     }
 }
