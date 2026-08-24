50dec68d203ddda44f932d6df6b7344fabafcb98
HotSpot2000-hub <HotSpot2000@gmx.net>
2026-08-24 14:53:09 +0200

    Update MainActivity.java

--- native/MainActivity.java
+++ native/MainActivity.java
@@ -20,4 +20,22 @@ public class MainActivity extends BridgeActivity {
             getBridge().getWebView().setOverScrollMode(View.OVER_SCROLL_NEVER);
         }
     }
+public void setVideoImmersive(boolean enabled) {
+    runOnUiThread(() -> {
+        View decorView = getWindow().getDecorView();
+
+        if (enabled) {
+            decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        } else {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+        }
+    });
+}
 }
